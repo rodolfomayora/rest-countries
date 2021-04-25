a2c5ac3e17dfe2293274d6ee479c62739b86f71f
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-04-24 21:22:08 -0400

    RC21: Solver reload country detail  page issue

--- src/views/CountryDetail/index.tsx
+++ src/views/CountryDetail/index.tsx
@@ -20,25 +20,7 @@ const CountryDetail: FC = () => {
   const countriesById: any = useSelector(selectAllCountriesById);
   const countryData: any = useSelector(selectCountryById(id));
 
-  const {
-    name,
-    nativeName,
-    flagImage,
-    region,
-    population,
-    subregion,
-    capital,
-    topLevelDomain,
-    currencies,
-    languages,
-    borderCountries
-  } = countryData;
-
   const mapArrayToText = (arr: Array<string>) => arr.join(', ');
-
-  const currenciesNames: string = mapArrayToText(currencies);
-
-  const languagesName: string = mapArrayToText(languages);
 
   return (
     <Layout>
@@ -47,99 +29,101 @@ const CountryDetail: FC = () => {
           <section className={style.sectionWrapper}>
             <BackToHomeButton/>
 
-            <div className={style.infoContainer}>
-              <div className={style.flagWrapper}>
-                <div className={style.flagLayer}>
-                  <div className={style.flagContent}>
-                    <img className={style.flagImage}
-                      src={flagImage}
-                      alt={`${name} flag`}
-                      width="200"
-                      height="100"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className={style.blockInfo}>
-                <div className={style.countryInfo}>
-                  <h2 className={style.countryName}>{name}</h2>
-                  
-                  <div className={style.countryData}>
-                    <div className={style.data}>
-                      <p>
-                        <span className={style.label}>
-                          Native Name:
-                        </span>{' '}
-                        {nativeName}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Population:
-                        </span>{' '}
-                        {population}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Region:
-                        </span>{' '}
-                        {region}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Sub Region: 
-                        </span>{' '}
-                        {subregion}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Capital:
-                        </span>{' '}
-                        {capital}
-                      </p>
-                    </div>
-
-                    <div className={style.data}>
-                      <p>
-                        <span className={style.label}>
-                          Top Level Domain:
-                        </span>{' '}
-                        {topLevelDomain}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Currences:
-                        </span>{' '}
-                        {currenciesNames}
-                      </p>
-                      <p>
-                        <span className={style.label}>
-                          Languajes:
-                        </span>{' '}
-                        {languagesName}
-                      </p>
+            {!!countryData && (
+              <div className={style.infoContainer}>
+                <div className={style.flagWrapper}>
+                  <div className={style.flagLayer}>
+                    <div className={style.flagContent}>
+                      <img className={style.flagImage}
+                        src={countryData.flagImage}
+                        alt={`${countryData.name} flag`}
+                        width="200"
+                        height="100"
+                      />
                     </div>
                   </div>
                 </div>
 
-                <section>
-                  <h3 className={style.borderSubtitle}>Border Countries:</h3>
-                  <div className={style.borderCountries}>
-                    {!!borderCountries.length ? (
-                      borderCountries.map((countryId: string) => (
-                        <BorderCountryButton countryId={countryId}
-                          key={countryId}
-                        >
-                          {countriesById[countryId].name}
-                        </BorderCountryButton>
-                      ))
-                    ) : (
-                      <div>It does not have Border Countries</div>
-                    )}
+                <div className={style.blockInfo}>
+                  <div className={style.countryInfo}>
+                    <h2 className={style.countryName}>{countryData.name}</h2>
+                    
+                    <div className={style.countryData}>
+                      <div className={style.data}>
+                        <p>
+                          <span className={style.label}>
+                            Native Name:
+                          </span>{' '}
+                          {countryData.nativeName}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Population:
+                          </span>{' '}
+                          {countryData.population}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Region:
+                          </span>{' '}
+                          {countryData.region}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Sub Region: 
+                          </span>{' '}
+                          {countryData.subregion}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Capital:
+                          </span>{' '}
+                          {countryData.capital}
+                        </p>
+                      </div>
+
+                      <div className={style.data}>
+                        <p>
+                          <span className={style.label}>
+                            Top Level Domain:
+                          </span>{' '}
+                          {countryData.topLevelDomain}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Currences:
+                          </span>{' '}
+                          {mapArrayToText(countryData.currencies ?? [])}
+                        </p>
+                        <p>
+                          <span className={style.label}>
+                            Languajes:
+                          </span>{' '}
+                          {mapArrayToText(countryData.languagesName ?? [])}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </section>
+
+                  <section>
+                    <h3 className={style.borderSubtitle}>Border Countries:</h3>
+                    <div className={style.borderCountries}>
+                      {!!countryData.borderCountries?.length ? (
+                        countryData.borderCountries.map((countryId: string) => (
+                          <BorderCountryButton countryId={countryId}
+                            key={countryId}
+                          >
+                            {countriesById[countryId].name}
+                          </BorderCountryButton>
+                        ))
+                      ) : (
+                        <div>It does not have Border Countries</div>
+                      )}
+                    </div>
+                  </section>
+                </div>
               </div>
-            </div>
+            )}
           </section>
         </Container>
       </main>
