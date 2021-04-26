d4fe47f16d6ef73aa6614e24c9f22dd92c385a94
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-04-26 06:23:36 -0400

    hotfix: add itemKey to virtualized list

--- src/components/SearchCountry/index.tsx
+++ src/components/SearchCountry/index.tsx
@@ -15,7 +15,7 @@ const SearchCountry: FC = () => {
   useEffect(() => {
     const debounceCountryName: number = window.setTimeout(() => {
       dispatch(setCountryName(input.trim().toLowerCase()))
-    },700)
+    },500)
 
     return () => window.clearInterval(debounceCountryName);
   },
