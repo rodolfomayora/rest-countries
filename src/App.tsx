a7bbb3034b3b9440162f4f080355fcee858de661
Rodolfo Mayora Pereda <rodolfomayora96@gmail.com>
2021-07-01 19:21:45 -0400

    RC27: add localStorage for theme store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import React, { FC } from 'react';
-import './assets/styles/styles.global.scss';
+import React, { FC, useEffect } from 'react';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectTheme } from './store/rootSelectors';
+import { fetchCountries, setTheme } from './store/rootActions';
 import { Home, CountryDetail, NoMatch404 } from './views';
-import { useDispatch } from 'react-redux';
-import { fetchCountries } from './store/rootActions';
+import './assets/styles/styles.global.scss';
 
 const App: FC = () => {
 
   const dispatch = useDispatch();
-  dispatch(fetchCountries());
+  useEffect(() => {
+    const initializeCountryGlobalState = () => dispatch(fetchCountries());
+    initializeCountryGlobalState();
+  },
+  [dispatch])
+  
+  useEffect(() => {
+    const getLocalStorageTheme = () => {
+      const storagedTheme: any = window.localStorage.getItem('theme');
+      if (!!storagedTheme) dispatch(setTheme(storagedTheme));
+    }
+
+    getLocalStorageTheme();
+  },
+  [dispatch])
+
+  const currentTheme = useSelector(selectTheme);
+  useEffect(() => {
+    const setLocalStorageTheme = () => {
+      try {
+        window.localStorage.setItem('theme', currentTheme);
+      } catch(error) {
+        console.error(error);
+      }
+    }
+
+    setLocalStorageTheme();
+  },
+  [currentTheme])
 
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/404" component={NoMatch404} />
-        <Route path="/:id" component={CountryDetail} />
+        <Route path="/CountryDetail/:id" component={CountryDetail} />
       </Switch>
     </BrowserRouter>
   );
