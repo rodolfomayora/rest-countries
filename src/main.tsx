d2d3bc7c5b1a0259953dae244cf56230e06773f0
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-09-21 15:13:43 -0400

    refactor(styles): reimplemented global styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
+import './style/global.scss';
 
 const queryClient = new QueryClient();
 
